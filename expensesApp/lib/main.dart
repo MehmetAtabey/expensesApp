74ea04a90cdada317da3ab73bdbf800f95b9a119
mehmetatabey <mehmet_atabey@yahoo.com>
2020-06-25 01:56:00 +0300

    modified

--- expensesApp/lib/main.dart
+++ expensesApp/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:intl/intl.dart';
 import 'package:expensesApp/transaction.dart';
 import 'package:flutter/material.dart';
 
@@ -23,7 +24,6 @@ class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
   final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
@@ -31,10 +31,10 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   final List<Transaction> transactions = [
     Transaction(
-        id: "t1", title: "ayakkabı", amount: 300.00, date: DateTime.now()),
-    Transaction(id: "t1", title: "tshirt", amount: 20.00, date: DateTime.now()),
+        id: "t1", title: "ayakkabı", amount: 300.25, date: DateTime.now()),
+    Transaction(id: "t1", title: "tshirt", amount: 20.12, date: DateTime.now()),
     Transaction(
-        id: "t1", title: "telefon", amount: 2500.00, date: DateTime.now())
+        id: "t1", title: "telefon", amount: 2500.45, date: DateTime.now())
   ];
   @override
   Widget build(BuildContext context) {
@@ -50,28 +50,50 @@ class _MyHomePageState extends State<MyHomePage> {
               width: double.infinity,
               height: 100,
               child: Card(child: Text("CHART!!"), elevation: 10)),
+          Card(
+            child: Container(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  TextField(decoration: InputDecoration(labelText: "Title")),
+                  TextField(decoration: InputDecoration(labelText: "Amount")),
+                  FlatButton(onPressed: () {}, child: Text("Add Transaction"))
+                ],
+              ),
+            ),
+          ),
           Column(
               children: transactions.map((tx) {
             return Card(
                 child: Row(
               children: <Widget>[
                 Container(
-                  padding: EdgeInsets.all(10),
+                    padding: EdgeInsets.all(10),
                     margin: EdgeInsets.all(20),
                     decoration: BoxDecoration(
-                      border: Border.all(width: 1, style: BorderStyle.solid),
+                      border: Border.all(width: 1, color: Colors.black45),
                     ),
                     child: Text(
-                      tx.amount.toString(),
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 15,
-                      ),
+                      '₺ ' + tx.amount.toString(),
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
                     )),
                 Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    Text(tx.title),
-                    Text(tx.date.toString()),
+                    Text(tx.title,
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15)),
+                    Text(
+                      DateFormat().format(tx.date),
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 10,
+                      ),
+                    ),
                   ],
                 )
               ],
