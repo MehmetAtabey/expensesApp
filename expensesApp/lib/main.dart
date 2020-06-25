89168c8873d0663ea49ea9fb4c9d5a2211480875
MehmetAtabey <mehmet_atabey@yahoo.com>
2020-06-25 18:53:02 +0300

    modified

--- expensesApp/lib/main.dart
+++ expensesApp/lib/main.dart
@@ -1,6 +1,10 @@
+import 'dart:math';
+
 import 'package:expensesApp/widgets/new_transaction.dart';
-import 'package:expensesApp/widgets/user_transactions.dart';
+import 'package:expensesApp/widgets/transaction_list.dart';
 import 'package:flutter/material.dart';
+
+import 'models/transaction.dart';
 
 void main() {
   runApp(MyApp());
@@ -20,16 +24,42 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
 
-  void startAddNewTransaction(BuildContext ctx)
-  {
-    showModalBottomSheet(context: ctx, builder:(_){
-return NewTransaction(addTx)
+ 
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  final List<Transaction> _userTransaction = [
+    Transaction(id: "t1", title: "ayakkabı", amount: 300.25, date: DateTime.now()),
+    Transaction(id: "t1", title: "tshirt", amount: 20.12, date: DateTime.now()),
+    Transaction(id: "t1", title: "telefon", amount: 2500.45, date: DateTime.now())
+  ];
+  void _addNewTransaction(String title, double amount) {
+    final newTx = new Transaction(
+        id: Random.secure().nextInt(99999).toString(),
+        title: title,
+        amount: amount,
+        date: DateTime.now());
+
+    setState(() {
+      _userTransaction.add(newTx);
     });
   }
 
-  
+
+
+  void _startAddNewTransaction(BuildContext ctx)
+  {
+
+    showModalBottomSheet(context: ctx, builder:(_){
+return NewTransaction(_addNewTransaction);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -53,12 +83,12 @@ return NewTransaction(addTx)
                 width: double.infinity,
                 height: 100,
                 child: Card(child: Text("CHART!!"), elevation: 10)),
-            UserTransactions()
+                TransactionList(_userTransaction)
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add, color: Colors.white), onPressed: () {}),
+          child: Icon(Icons.add, color: Colors.white), onPressed: ()=> _startAddNewTransaction(context) ),
     );
   }
 }
